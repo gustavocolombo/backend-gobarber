98dcad570bba06bb0a1822cd0643a354a18f6a28
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-07-20 20:50:55 -0300

    backend inicial gobarber

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,37 @@
+import Appointment from '../models/Appointment';
+import { isEqual } from 'date-fns';
+
+interface CreateAppointmentDTO{
+  provider: string;
+  date: Date;
+}
+
+class AppointmentsReposirory{
+  private appointments : Appointment[];
+
+    constructor(){
+      this.appointments = [];
+    }
+
+      public create({provider, date}: CreateAppointmentDTO) : Appointment{
+        const appointment =  new Appointment(provider,  date);
+        
+        this.appointments.push(appointment);
+
+        return appointment;
+      }
+
+      public findByDate(date: Date): Appointment | null {
+        const findAppointmentInSameDate = this.appointments.find (appointment => 
+          isEqual( date, appointment.date )
+        );
+
+        return findAppointmentInSameDate || null;
+      }
+
+      public listAll() : Appointment[]{
+        return this.appointments;        
+      }
+}
+
+export default AppointmentsReposirory;
